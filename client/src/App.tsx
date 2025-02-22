69886bdc3d70dfca84764857555c7681e46810fe
Sudi <sudisimbadav@gmail.com>
2025-02-22 21:10:50 +0200

    feat: add activity type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [activities, setActivities] = useState<Activity[]>([])
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/activities")
+      .then((res) => res.json())
+      .then(data =>setActivities(data))
+  }, [])
   return (
-      <h3>TogetherHub</h3>
+      <div>
+        <h3>TogetherHub</h3>
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
+      </div>
   )
 }
 
