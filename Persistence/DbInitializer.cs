336f5753ce25676fbf74e98b76109ed9dd8359df
Sudi <sudisimbadav@gmail.com>
2025-02-22 18:34:02 +0200

    Initial create

---
+++ Persistence/DbInitializer.cs
@@ -0,0 +1,117 @@
+using Domain;
+
+namespace Persistence;
+
+public class DbInitializer
+{
+    public static async Task SeedData(AppDbContext context)
+    {
+        if(context.Activities.Any()) return;
+
+        var activities = new List<Activity>
+        {
+            new()
+            {
+                Title = "Past Activity 1",
+                Date = DateTime.Now.AddMonths(-2),
+                Description = "Activity 2 months ago",
+                Category = "drinks",
+                City = "London",
+                Venue = "Pub",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Past Activity 2",
+                Date = DateTime.Now.AddMonths(-1),
+                Description = "Activity 1 month ago",
+                Category = "culture",
+                City = "Paris",
+                Venue = "Louvre",
+                Latitude = 48.8566,
+                Longitude = 2.3522
+            },
+            new()
+            {
+                Title = "Future Activity 1",
+                Date = DateTime.Now.AddMonths(1),
+                Description = "Activity 1 month in future",
+                Category = "culture",
+                City = "London",
+                Venue = "Natural History Museum",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Future Activity 2",
+                Date = DateTime.Now.AddMonths(2),
+                Description = "Activity 2 months in future",
+                Category = "music",
+                City = "London",
+                Venue = "O2 Arena",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Future Activity 3",
+                Date = DateTime.Now.AddMonths(3),
+                Description = "Activity 3 months in future",
+                Category = "drinks",
+                City = "London",
+                Venue = "Another pub",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Future Activity 4",
+                Date = DateTime.Now.AddMonths(4),
+                Description = "Activity 4 months in future",
+                Category = "drinks",
+                City = "London",
+                Venue = "Yet another pub",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Future Activity 5",
+                Date = DateTime.Now.AddMonths(5),
+                Description = "Activity 5 months in future",
+                Category = "drinks",
+                City =  "London",
+                Venue = "Just another pub",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Future Activity 6",
+                Date = DateTime.Now.AddMonths(6),
+                Description = "Activity 6 months in future",
+                Category = "music",
+                City = "London",
+                Venue = "Roundhouse",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            },
+            new()
+            {
+                Title = "Future Activity 7",
+                Date = DateTime.Now.AddMonths(7),
+                Description = "Activity 2 months ago",
+                Category = "travel",
+                City = "London",
+                Venue = "Somewhere on the Thames",
+                Latitude = 51.5074,
+                Longitude = 0.1278
+            }
+        };
+        
+        await context.Activities.AddRangeAsync(activities);
+        await context.SaveChangesAsync();
+    }
+}
